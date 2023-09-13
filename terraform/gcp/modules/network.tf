563f41f844d1cf0f0fd8fd2ad55c5ca15a0fb267
rameshpattipaka <144959699+rameshpattipaka@users.noreply.github.com>
2023-09-13 18:19:03 -0500

    Create network.tf

---
+++ terraform/gcp/modules/network.tf
@@ -0,0 +1,17 @@
+resource "google_compute_network" "sample_vpc" {
+  name = "intuitive-vpc"
+}
+
+
+resource "google_compute_subnetwork" "sample_subnet" {
+  name          = var.subnet_name
+  network       = google_compute_network.sample_vpc.name
+  ip_cidr_range = var.cidr_range 
+  region        = var.region  
+}
+
+resource "google_compute_network_interface" "sample_nic" {
+  count = var.nic_count
+  name  = "${var.nic_name_region_prefix}-${var.nic_name}-0${count.index+1}"
+  network = google_compute_network.sample_vpc.name  
+}
