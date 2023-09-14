96b4cfa7da25dc3fd683b68cbfaec9be74ca599d
rameshpattipaka <144959699+rameshpattipaka@users.noreply.github.com>
2023-09-14 08:48:02 -0500

    Update network.tf

--- terraform/aws/modules/network.tf
+++ terraform/aws/modules/network.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "intuitive_vpc" {
 }
 
 resource "aws_internet_gateway" "intuitive_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.intuitive_vpc.id
 
   tags = {
     Name = "intuitive-igw"
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "intuitive_igw" {
 resource "aws_subnet" "public_subnets" {
   vpc_id                  = aws_vpc.intuitive_vpc.id
   cidr_block              = var.cidr_block
-  availability_zone       = var.az
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
 
   tags = {
